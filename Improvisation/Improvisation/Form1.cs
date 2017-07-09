27ed4e541b90885411f328674fdc152a6c467acf
Antoine <antoine.boulange33@gmail.com>
2017-07-09 18:16:39 +0200

    Version 1.1

--- Improvisation/Improvisation/Form1.cs
+++ Improvisation/Improvisation/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Improvisation
 {
@@ -25,6 +26,25 @@ namespace Improvisation
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Point[] pointsArray =
+            {
+                new Point(50, 0),
+                new Point(450, 0),
+                new Point(440, 10),
+                new Point(430, 20),
+                new Point(70, 20),
+                new Point(60, 10)
+            };
+
+            GraphicsPath poly = new GraphicsPath();
+            poly.AddPolygon(pointsArray);
+
+            Pen border = new Pen(Color.LightBlue, 4);
+            e.Graphics.DrawPolygon(border, pointsArray);
         }
     }
 }
